d1e3a653a76642b4f77bcfc6a2b9bba883ada503
mkeloo <mkeloo@ufl.edu>
2022-08-05 18:15:38 -0400

    Added filter options to js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-// 
+const filter_btns = document.querySelectorAll(".filter-btn");
+
+filter_btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({ filter: filterValue });
+  })
+);
 
 $(".grid").isotope({
-    itemSelector: ".grid-item",
-    layoutMode: "fitRows",
-    transitionDuration: "0.6s",
+  itemSelector: ".grid-item",
+  layoutMode: "fitRows",
+  transitionDuration: "0.6s",
 });
- 
