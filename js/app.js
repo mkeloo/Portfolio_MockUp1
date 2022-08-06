7fa59e69cd74bed2bb54ce0f8e1e86d87bd644f6
mkeloo <mkeloo@ufl.edu>
2022-08-05 21:17:13 -0400

    Finished About Section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
 
 filter_btns.forEach((btn) =>
   btn.addEventListener("click", () => {
@@ -16,3 +18,18 @@ $(".grid").isotope({
   layoutMode: "fitRows",
   transitionDuration: "0.6s",
 });
+
+window.addEventListener("scroll", () => {
+  skillsEffect();
+});
+
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsEffect() {
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
