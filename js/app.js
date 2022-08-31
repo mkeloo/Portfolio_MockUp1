c4aa7d0ec8735174b817bbb5fbd15b1f365785a1
mkeloo <mkeloo@ufl.edu>
2022-08-31 02:38:17 -0400

    Final Commit

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ const records_numbers = document.querySelectorAll(".number")
 const footer_input = document.querySelector(".footer-input")
 const hamburger_menu = document.querySelector(".hamburger-menu")
 const navbar = document.querySelector("header nav")
-const links = document.querySelector(".links a")
+const links = document.querySelectorAll(".links a")
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus")
 })
 
-footer_input.addEventListener("blue", () => {
+footer_input.addEventListener("blur", () => {
   if (footer_input.value != "") return
   footer_input.classList.remove("focus")
 })
@@ -72,7 +72,7 @@ function countUp() {
     const updateCount = () => {
       let currentNum = +numb.innerText
       let maxNum = +numb.dataset.num
-      let speed = 500
+      let speed = 100
       const increment = Math.ceil(maxNum / speed)
 
       if (currentNum < maxNum) {
@@ -87,17 +87,15 @@ function countUp() {
   })
 }
 
-const mySwiper = new Swiper(".swiper-container", {
+var mySwiper = new Swiper(".swiper-container", {
   speed: 1100,
   slidesPerView: 1,
   loop: true,
   autoplay: {
-    delay: 4000,
+    delay: 5000,
   },
-
-  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
   },
 })
